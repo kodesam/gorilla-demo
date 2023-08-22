a18bcc67b62666f63cfcb036933f654dc6f1bbff
Shishir Patil <shishirpatil@berkeley.edu>
2023-08-22 00:44:33 -0700

    First Commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ---
 title: Gradio Demo
-emoji: 🐢
+emoji: 🦍
 colorFrom: indigo
 colorTo: blue
 sdk: gradio
@@ -10,4 +10,22 @@ pinned: false
 license: apache-2.0
 ---
 
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+# Gorilla: Large Language Model Connected with Massive APIs [[Project Website](https://shishirpatil.github.io/gorilla/)]
+
+
+<img src="https://github.com/ShishirPatil/gorilla/blob/gh-pages/assets/img/logo.png" width=50% height=50%>
+
+:rocket: [Gorilla Github](https://github.com/ShishirPatil/gorilla/)**
+
+**🟢 Gorilla is Apache 2.0** You can use Gorilla commercially with no obligations! :golf:  
+ 
+:computer: Use [Gorilla in your CLI](https://github.com/gorilla-llm/gorilla-cli) with `pip install gorilla-cli`
+
+**:newspaper_roll: Checkout our paper!** [![arXiv](https://img.shields.io/badge/arXiv-2305.15334-<COLOR>.svg?style=flat-square)](https://arxiv.org/abs/2305.15334)
+
+**:wave: Join our Discord!** [![Discord](https://img.shields.io/discord/1111172801899012102?label=Discord&logo=discord&logoColor=green&style=flat-square)](https://discord.gg/SwTyuTAxX3)
+
+
+`Gorilla` enables LLMs to use tools by invoking APIs. Given a natural language query, Gorilla comes up with the semantically- and syntactically- correct API to invoke. With Gorilla, we are the first to demonstrate how to use LLMs to invoke 1,600+ (and growing) API calls accurately while reducing hallucination. We also release APIBench, the largest collection of APIs, curated and easy to be trained on! Join us, as we try to expand the largest API store and teach LLMs how to write them! Hop on our Discord, or open a PR, or email us if you would like to have your API incorporated as well. 
+
+Gorilla is an Open-source project, and this gradio app was built by [TanmayDoesAI](https://github.com/TanmayDoesAI)
---
+++ app.py
@@ -0,0 +1,112 @@
+# Copyright 2023 https://github.com/ShishirPatil/gorilla
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import gradio as gr
+import openai
+import re
+
+# There is no need for an API key let the following be as is
+openai.api_key = "EMPTY"
+
+# Set up the API base
+openai.api_base = "http://zanino.millennium.berkeley.edu:8000/v1"
+# If there is any issue try using
+# openai.api_base = "http://34.132.127.197:8000/v1"
+
+# Define function to get Gorilla response
+def get_gorilla_response(prompt, model="gorilla-7b-hf-v1"):
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return completion.choices[0].message.content
+
+# Define function to parse output
+def parse_output(text, model):
+    if model == "gorilla-7b-hf-v1":
+        components = {}
+        components['domain'] = text.split("<<<domain>>>:")[1].split("<<<api_call>>>")[0].strip()
+        components['api_call'] = text.split("<<<api_call>>>:")[1].split("<<<api_provider>>>")[0].strip()
+        components['api_provider'] = text.split("<<<api_provider>>>:")[1].split("<<<explanation>>>")[0].strip()
+        components['explanation'] = text.split("<<<explanation>>>:")[1].split("<<<code>>>")[0].strip()
+        components['code'] = text.split("<<<code>>>:")[1].strip()
+        return components
+    elif model == "gorilla-mpt-7b-hf-v0":
+        keys_to_remove = ['api_call', 'api_provider', 'explanation', 'code']
+        x = text.split(":")
+        x.pop(0)
+        for i in range(len(x)):
+            for key in keys_to_remove:
+                x[i] = x[i].replace(f'{key}','').replace(f", '{key}'", '').replace(f", '{key}':", '').replace(f"'{key}':", '').replace('''\\"''','''"''').replace('''"\\''','''"''').replace("""\'""","""'""").replace("""'\\""","""'""")
+        components = {
+            'domain': x[0].strip("' ").replace("\n<<<","").replace('"','').replace('<','').replace('>',''),
+            'api_call': x[1].strip("' ").replace("\n<<<","").replace('<','').replace('>',''),
+            'api_provider': x[2].strip("' ").replace("\n<<","").replace('<','').replace('>',''),
+            'explanation': x[3].strip("' ").replace(r'\n', '\n').replace('<','').replace('>',''),
+            'code': x[4].strip("' ").replace(r'\n', '\n').replace('<','').replace('>','')
+        }
+        return components
+    elif model == "gorilla-7b-th-v0":
+        x = text.split(":")
+        keys_to_remove = ['api_call', 'api_provider', 'explanation', 'code']
+        x.pop(0)
+        for i in range(len(x)):
+            for key in keys_to_remove:
+                x[i] = x[i].replace(f", '{key}'", '').replace(f", '{key}':", '').replace(f"'{key}':", '').replace('''\\"''','''"''').replace('''"\\''','''"''').replace("""\'""","""'""").replace("""'\\""","""'""")
+        components = {
+            'domain': x[0].strip("' "),
+            'api_call': x[1].strip("' "),
+            'api_provider': x[2].strip("' "),
+          'explanation': x[3].strip("' ").replace(r'\n', '\n'),
+          'code': x[4].strip("' ").replace(r'\n', '\n')
+        }
+        return components
+
+# Define the function for the interface
+def parse_and_display(prompt, model):
+    text = get_gorilla_response(prompt, model)
+    components = parse_output(text, model)
+    domain = components['domain']
+    api_call = components['api_call']
+    api_provider = components['api_provider']
+    explanation = components['explanation']
+    code = components['code']
+    return domain, api_call, api_provider, explanation, code
+
+# Define example prompts
+examples = [
+    ["I would like to translate 'I feel very good today.' from English to Chinese.","gorilla-7b-hf-v1"],
+    ["I would like to translate from English to Chinese.","gorilla-7b-th-v0"],
+    ["I would like to translate from English to German.","gorilla-mpt-7b-hf-v0"]
+]
+
+# Create the Gradio interface
+iface = gr.Interface(
+    fn=parse_and_display,
+    inputs=["text", gr.components.Dropdown(["gorilla-7b-hf-v1", "gorilla-mpt-7b-hf-v0", "gorilla-7b-th-v0"], label="Model")],
+    outputs=[
+        gr.components.Textbox(label="Domain"),
+        gr.components.Textbox(label="API Call"),
+        gr.components.Textbox(label="API Provider"),
+        gr.components.Textbox(label="Explanation"),
+        gr.components.Code(label="Code")
+    ],
+    title="Gorilla Gradio Explorer",
+    description="Gorilla is an LLM that can pick the right API for your tasks. Check out the examples below. Learn more at gorilla.cs.berkeley.edu",
+    examples=examples,
+)
+
+# Launch the interface and get the public gradio link
+iface.launch(share=True,debug=True)
